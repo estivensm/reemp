aee9b6bab113c9f507854cbefdd2af13eab9e893
alejovelez10 <alejovelez10@gmail.com>
2019-01-30 14:23:46 -0500

    organizano

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,7 +5,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   storage (Rails.env.production? ? :fog : :file)
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
