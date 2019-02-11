182f8208c1a60dfd3430b72a467b6063e3042659
alejovelez10 <alejovelez10@gmail.com>
2019-02-11 13:20:35 -0500

    contact

--- app/mailers/contacto_mailer.rb
+++ app/mailers/contacto_mailer.rb
@@ -1,6 +1,6 @@
 class ContactoMailer < ApplicationMailer
 	def contacto(contact) 
 		@contact = contact
-		mail(to: "salazar-moncada-estiven@hotmail.com", subject: "Alguien nos contacto")
+		mail(to: "reempers@reemp.co", subject: "Alguien nos contacto")
 	end
 end
