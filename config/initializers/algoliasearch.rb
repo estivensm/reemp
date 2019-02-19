5fcae6e4bab620e2430094992d5d858a34d1b7a9
alejovelez10 <alejovelez10@gmail.com>
2019-02-19 18:04:47 -0500

    ini

---
+++ config/initializers/algoliasearch.rb
@@ -0,0 +1,11 @@
+AlgoliaSearch.configuration = { application_id: 'DJ3JJMGZSP', api_key: '0f9abe035e333810031a60c33a56907c' }
+
+AlgoliaSearch.configuration = {
+  application_id: 'DJ3JJMGZSP',
+  api_key: '0f9abe035e333810031a60c33a56907c',
+  connect_timeout: 2,
+  receive_timeout: 30,
+  send_timeout: 30,
+  batch_timeout: 120,
+  search_timeout: 5
+}
